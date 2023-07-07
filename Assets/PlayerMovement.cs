bd8d35fddaba23a7d7264607218510fe53c7c6ef
Josh Pattman <Josh.pattman@gmail.com>
2023-07-07 22:50:22 +0100

    thickened arms

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    CharacterController cc;
+
+    Vector2 rotation = Vector2.zero;
+	public float rotationSpeed = 3;
 
     void Awake()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
     
     void Start()
@@ -17,6 +21,28 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        
+        Vector3 moveVec = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVec += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVec -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVec -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVec += transform.right;
+        }
+
+        cc.Move(moveVec.normalized * Time.deltaTime * 5f);
+
+        rotation.y += Input.GetAxis("Mouse X");
+		rotation.x += -Input.GetAxis("Mouse Y");
+		transform.eulerAngles = (Vector2)rotation * rotationSpeed;
     }
 }
